5ac28b44ccf19b9b6c79babc26ea2f3740d510b3
David Kheyman <david.kheyman@cloudreach.com>
2017-11-02 14:43:23 -0400

    qq

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo su
-docker pull dkheyman/trial-repo:${BUILD_ENV}
+docker pull dkheyman/trial-repo:$BUILD_ENV
 docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker run --rm -d -p 8080:8080 dkheyman/trial-repo:${BUILD_ENV}
+docker run --rm -d -p 8080:8080 dkheyman/trial-repo:$BUILD_ENV
