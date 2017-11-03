d3ac0e7f85bcf117e77803a8c0eeaf797e2e8171
David Kheyman <david.kheyman@cloudreach.com>
2017-11-03 11:34:48 -0400

    qq

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo su
 export BUILD_ENV=`cat /etc/env`
 docker pull dkheyman/trial-repo:$BUILD_ENV
-docker stop $(docker ps -q)
-docker rm $(docker ps -a -q)
+export NUM_CONTAINERS=`docker ps -q | wc -l | xargs`
+if [$NUM_CONTAINERS > 0]
+then
+  docker stop $(docker ps -q) && docker rm $(docker ps -a -q)
+fi
 docker run --rm -d -p 8080:8080 dkheyman/trial-repo:$BUILD_ENV
